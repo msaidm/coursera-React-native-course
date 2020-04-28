5852c03d6ccad73807a616d961bb5719da208bd0
mohammed <msaidm@windowslive.com>
2020-04-29 01:54:14 +0200

    Components Part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,12 +7,13 @@ function Menu(props){
     const renderMenuItem =({item,index})=>{
         return(
             <ListItem
-            key = {index}
-            title = {item.name}
-            subtitle = {item.description}
-            hideChevron={true}
-            leftAvatar={{source: require('./images/uthappizza.png')}}
-            />
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    hideChevron={true}
+                    onPress={() => props.onPress(item.id)}
+                    leftAvatar={{ source: require('./images/uthappizza.png')}}
+                  />
         );
     }
     return(
