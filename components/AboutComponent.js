24dccfc058bde36494388b15d34ed94c602e0063
mohammed <msaidm@windowslive.com>
2020-05-07 17:19:32 +0200

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,7 @@ import { LEADERS } from '../shared/leaders';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -36,9 +37,10 @@ const mapStateToProps = state => {
             );
         };
 
-        return(
-            <ScrollView>
-                <Card title='Our History'>
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <Card title='Our History'>
                     <Text style={{margin: 5}}>Started in 2010, Ristorante con Fusion quickly established 
                     itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion 
                     cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  
@@ -48,16 +50,60 @@ const mapStateToProps = state => {
                     a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time 
                     the world's best cuisines in a pan.</Text>     
                 </Card>
-                <Card title='Corporate Leadership'>
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                   <Card title='Our History'>
+                    <Text style={{margin: 5}}>Started in 2010, Ristorante con Fusion quickly established 
+                    itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion 
+                    cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  
+                    Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your 
+                    plate the next time you visit us.</Text>
+                    <Text style={{margin: 5}}>The restaurant traces its humble beginnings to The Frying Pan, 
+                    a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time 
+                    the world's best cuisines in a pan.</Text>     
+                </Card>
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <Card title='Our History'>
+                    <Text style={{margin: 5}}>Started in 2010, Ristorante con Fusion quickly established 
+                    itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion 
+                    cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  
+                    Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your 
+                    plate the next time you visit us.</Text>
+                    <Text style={{margin: 5}}>The restaurant traces its humble beginnings to The Frying Pan, 
+                    a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time 
+                    the world's best cuisines in a pan.</Text>     
+                </Card>
+                    <Card
+                        title='Corporate Leadership'>
                     <FlatList 
                         data={this.props.leaders.leaders}
-                        renderItem={renderLeadersItem}
+                        renderItem={renderLeader}
                         keyExtractor={item => item.id.toString()}
                         />
-                </Card>
-            </ScrollView>
-        );
+                    </Card>
+                </ScrollView>
+            );
+        }
     }
+        
+    
 }
 
 export default connect(mapStateToProps)(About);
